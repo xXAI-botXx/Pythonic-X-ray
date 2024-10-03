c88d5fb5bbf1f9852c8202bff0ea00e1ebca6d89
Tobia <tobia2761@gmail.com>
2024-10-03 11:41:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import code_analyzer as ca
-
 import os
 
+def main(FILE_NAME, PATH):
+    for cur_root, cur_dirs, cur_files in os.walk(PATH):
+        if FILE_NAME in cur_files:
+            ca.analyse_code(os.path.join(cur_root, FILE_NAME), True, True)
+            break
+    
+    
+if __name__ == "__main__":
+    # CHANGE THESE 2 VARS
+    FILE_NAME = "heapsort.py"
+    PATH = "./"
+    main(FILE_NAME, PATH)
 
-# check if one file or many
-
-# one file:
-ca.analyse_code('./res/heapsort.py', True, True)
-
-# many files
-# go trough all files and find the .py files and add to an str
-path = ""
-complete_py_str = ""
-entities = os.listdir(path)
-# recursive is better for that
-for entity in entities:
-    pass
